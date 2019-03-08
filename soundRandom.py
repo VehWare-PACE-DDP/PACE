55a0a0cadc77d203458b53a9850be75b02fb5436
VehWare-PACE-DDP <44152115+VehWare-PACE-DDP@users.noreply.github.com>
2019-03-08 13:58:56 -0500

    Random sound function called in a multithreaded way.

--- soundRandom.py
+++ soundRandom.py
@@ -1,63 +1,185 @@
-#!usr/bin/env python
-#coding=utf-8
-
-import pyaudio
-import wave
-import random
-from datetime import datetime
+import soundRandom
 import time
-import datetime
+import sys
+import pygame
+import pandas as pd
+import csv
+import matplotlib
+import threading
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-class soundRandom:
 
-    def randSound(self):
-        fh = open("log.txt","w")
+red = (255,0,0)
+black = (0,0,0)
+green = (0,255,0)
 
-        #print('Press Ctrl-C to quit.')
-        try:
+class rectObj:
+	xCoord = 0
+	color = red
 
-            while True:
-                random.seed(datetime.datetime.now())
-                percentage_chance = random.uniform(0.0, 1.0)
-        #print 'perc. chance =  ' + str(percentage_chance)
-                chance = random.random()
-                if chance < percentage_chance:
-            #print 'if statement i chance = ' + str(chance)
-            #define stream chunk
-                    chunk = 1024
-
-            #open a wav format music
-                    f = wave.open( './smashingSound.wav', 'rb' )
-            #instantiate PyAudio
-                    p = pyaudio.PyAudio()
-            #open stream
-                    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-                            channels = f.getnchannels(),
-                            rate = f.getframerate(),
-                            output = True)
-            #read data
-                    data = f.readframes(chunk)
-
-            #play stream
-                    while data:
-                        stream.write(data)
-                        data = f.readframes(chunk)
-            #logtoafile
-                    millis = int(round(time.time() * 1000))
-                    t =str(millis)
-                    fh.write("%s\n"%t)
-                    fh.flush()
-            #stop stream
-                    stream.stop_stream()
-                    stream.close()
-            #close PyAudio
-                    f.close()
+(width, height) = (1000, 300)
+screen_color = (255, 255, 255)
+screen = pygame.display.set_mode((width, height))
+screen.fill(screen_color)
+pygame.display.set_caption('Optokinetic Tape Test')
+pygame.display.flip()
 
 
 
-                else:
-                    time.sleep(random.randint(5,10)) #randomly wait to 5 to 20 seconds
+numBlocks = int(width/100)+2
+blockList = []
 
-            fh.close()
-        except KeyboardInterrupt:
-            print ('\n')
+count = 0
+toggle = 0
+# 0 -> red
+# 1 -> black
+for x in range(numBlocks):
+    tempBlock = rectObj()
+
+    if toggle == 0 :
+        tempBlock.xCoord = (x*100)-100
+        toggle = 1
+        blockList.append(tempBlock)
+        continue
+
+    if toggle == 1 :
+        tempBlock.xCoord = (x*100)-100
+        tempBlock.color = black
+        count += 1
+        blockList.append(tempBlock)
+        if count == 3 :
+            count = 0
+            toggle = 0
+
+
+
+surf = pygame.Surface((300,150))
+surf.fill((255,255,255))
+pygame.draw.rect(surf, green, (0, 0, 300, 150), 7)
+screen.blit(surf, (350, 75))
+
+blockFile = open("blockPos.txt", "w")
+eyeFile = open("eyePos.txt", "w")
+dfBlock=pd.DataFrame()
+dfEye=pd.DataFrame() 
+
+
+
+running = True
+speed = 5
+runs = 0
+forward = 1
+rowNum = 1
+start_time = time.time()
+
+soundObj = soundRandom.soundRandom()
+t1 = threading.Thread(target=soundObj.randSound) # Function name without paranthesis, with paranthesis you CALL it and it gets block.
+t1.setDaemon(True)
+t1.start()
+
+while running:
+
+	
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	#sets num runs in each direction and flips direction when needed
+	if runs == 500:
+		if forward == 0:
+			running = False
+		runs = 0
+		forward = 0
+	if forward == 0:
+		if speed > 0:
+			time.sleep(.5)
+			speed = speed*(-1)
+
+#want to track when block in in xCoords 350 - 650
+	tempBlock = rectObj()
+	for block in blockList:
+		pygame.draw.rect(screen, block.color, (block.xCoord, 100, 100, 100))
+
+		if block.xCoord > 350 and block.xCoord < 650 and block.color == red:
+			tempBlock = block
+
+		block.xCoord += speed
+		if block.xCoord == 1100:
+			block.xCoord = -100
+		if block.xCoord == -200:
+			block.xCoord = 1000
+
+
+        #sleep needed to fine tune speed depending on system
+	time.sleep(.005)
+	pygame.display.update()
+	
+	#updateTime = time.time()
+
+
+	blockFile.write("Block in Focus Zone at Coords:\n")
+	#if tempBlock.xCoord  < 350:
+	if tempBlock.xCoord > 350 and tempBlock.xCoord < 650:
+		#blockFile.write("%i %s %i %s %i\n" % (tempBlock.xCoord, "to", (tempBlock.xCoord+100), "at time:", (time.time() - start_time)))
+		dfBlock.at[rowNum, 'X coordinate'] = tempBlock.xCoord
+		dfBlock.at[rowNum, 'Y coordinate'] = tempBlock.xCoord+100
+		dfBlock.at[rowNum, 'Time'] = (time.time() - start_time)
+		
+	mouseX,mouseY = pygame.mouse.get_pos()
+	#eyeFile.write("%i%s%i %s %i\n" % (mouseX, ",", mouseY, "at time:", (time.time() - start_time)))
+	dfEye.at[rowNum, 'X coordinate'] = mouseX
+	dfEye.at[rowNum, 'Y coordinate'] = mouseY
+		
+	if (mouseX in range(350,651)):
+		dfEye.at[rowNum, 'Bool'] = 1
+	else:
+		dfEye.at[rowNum, 'Bool'] = 0
+
+	dfEye.at[rowNum, 'Time'] = (time.time() - start_time)
+		#print(mouseX,mouseY)
+
+	#elif tempBlock.xCoord > 550:
+		#blockFile.write("%i %s %i %s %i\n" % (tempBlock.xCoord, "to", 650, "at time:", (time.time() - start_time)))
+		#dfBlock.at[rowNum, 'X coordinate'] = tempBlock.xCoord
+		#dfBlock.at[rowNum, 'Y coordinate'] = tempBlock.xCoord+100
+		#dfBlock.at[rowNum, 'Time'] = (time.time() - start_time)
+	#else:
+		#blockFile.write("%i %s %i %s %i\n" % (tempBlock.xCoord, "to", (tempBlock.xCoord+100), "at time:", (time.time() - start_time)))
+		#dfBlock.at[rowNum, 'X coordinate'] = tempBlock.xCoord
+		#dfBlock.at[rowNum, 'Y coordinate'] = tempBlock.xCoord+100
+		#dfBlock.at[rowNum, 'Time'] = (time.time() - start_time)
+
+
+	runs += 1
+	rowNum +=1
+
+print()
+#print('Block dataframe results:')
+#print(dfBlock)
+print()
+#print('Eye position dataframe results:')
+#print(dfEye)
+
+dfBlock.to_csv('Block_Log.csv', encoding='utf-8')
+dfEye.to_csv('Cursor_Log.csv', encoding='utf-8')
+
+
+
+#plt.plot(dfEye.loc[:, 'Time'], dfEye.loc[:, 'Bool'], marker='*',linestyle='-',label='Test Results')
+#plt.xlabel = 'Time'
+#plt.ylabel = 'Accuracy'
+#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
