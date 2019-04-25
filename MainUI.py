2da5ab0d617b741c776c81a99114254026048014
VehWare-PACE-DDP <44152115+VehWare-PACE-DDP@users.noreply.github.com>
2019-04-24 20:24:49 -0400

    Latest version of GUI for the experiment.

--- MainUI.py
+++ MainUI.py
@@ -3,6 +3,8 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from opto import opto
+from LoginScreen import LoginScreen
+from kivy.uix.popup import Popup
 
 class MyApp(App):
 
@@ -11,12 +13,21 @@ class MyApp(App):
 
 
         self.title = "PACE"
-        self.obj = opto()
+
+        self.objOpto = opto()
+
+        self.objLogin = LoginScreen()
+
+        self.demoLayout = self.objLogin.build()
+
+        self.popup = Popup(title='Participant Information', content = self.demoLayout)
+        self.objLogin.getPopup(self.popup)
 
         layout = BoxLayout(orientation = 'vertical')
         btn = Button(text = 'Start Optokinetic Test', background_normal = "", border = (10,10,10,10), background_color = (1, .3, .4, .85))
         btn2 = Button(text = 'Enter Participant Information')
-        btn.bind(on_press = self.obj.runTest)
+        btn.bind(on_press = self.objOpto.runTest)
+        btn2.bind(on_press = self.popup.open)
 
         layout.add_widget(btn2)
         layout.add_widget(btn)
@@ -25,6 +36,9 @@ class MyApp(App):
 
         return layout
 
+    def getpopup(self):
+        return self.popup
+
 
 if __name__ == '__main__':
     MyApp().run()
@@ -32,7 +46,7 @@ if __name__ == '__main__':
 
 
 '''
-First Button: Pop Up Window for Form
+First Button: Enter Login Screen
 Second Button: Start the Optokinetic Test
 
 '''
